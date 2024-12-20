1a2ee242e1e262d122f4c1dc10af0fd7a0e92898
spshreyah <spshreyash2002@gmail.com>
2024-12-21 01:56:42 +0530

    Description of the changes

--- E_Commerce_Cart/src/main/java/org/project/services/ProductCatService.java
+++ E_Commerce_Cart/src/main/java/org/project/services/ProductCatService.java
@@ -7,9 +7,10 @@ import org.project.models.ProductCatModel;
 public interface ProductCatService {
 
 	 boolean isProductCat(ProductCatModel procat);
-//	 List<ProductCatModel> getAllProductCategories();  // Method to fetch all product categories
+
+//	 get all categories  ==>
 	 
-	 List<ProductCatModel> getAllProductCats();  // New method to get all categories
+	 List<ProductCatModel> getAllProductCats();  
 	 
-	  boolean addProductCat(ProductCatModel procat);  // Add this method
+	  boolean addProductCat(ProductCatModel procat); 
 }
