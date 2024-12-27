8409d0333e61c3666adbb2a3958a1cfe1757b427
spshreyah <spshreyash2002@gmail.com>
2024-12-27 12:30:56 +0530

    ok

--- E_Commerce_Cart/src/main/java/org/project/services/LoginService.java
+++
@@ -1,9 +0,0 @@
-package org.project.services;
-
-import org.project.models.LoginModel;
-
-public interface LoginService {
-
-	public boolean isUser(LoginModel login);
-	 public boolean registerUser(LoginModel login);
-}
